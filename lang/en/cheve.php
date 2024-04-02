62624ff3ad2cad8b6726733810c13cbf6a139572
Kevin <vnctkevin@gmail.com>
2024-04-02 15:02:45 +0700

    initial commit

---
+++ lang/en/cheve.php
@@ -0,0 +1,53 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+
+/**
+ * English strings for cheve
+ *
+ * You can have a rather longer description of the file as well,
+ * if you like, and it can span multiple lines.
+ *
+ * @package    mod_cheve
+ * @copyright  2019 Richard Jones richardnz@outlook.com
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @see https://github.com/moodlehq/moodle-mod_cheve
+ * @see https://github.com/justinhunt/moodle-mod_cheve
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['modulename'] = 'cheve';
+$string['modulenameplural'] = 'cheves';
+$string['modulename_help'] = 'Use the cheve module for... | The cheve module allows...';
+$string['cheve:addinstance'] = 'Add a new cheve';
+$string['cheve:submit'] = 'Submit cheve';
+$string['cheve:view'] = 'View cheve';
+$string['chevefieldset'] = 'Custom example fieldset';
+$string['chevename'] = 'cheve Name';
+$string['chevename_help'] = 'This is the content of the help tooltip associated with the chevename field. Markdown syntax is supported.';
+$string['cheve'] = 'cheve';
+$string['pluginadministration'] = 'cheve administration';
+$string['pluginname'] = 'cheve';
+$string['nocheves'] = 'No instances';
+
+// Mod form specific cheve settings.
+$string['title'] = 'Activity Title: ';
+$string['vidtype'] = 'Sync/Async';
+$string['lang'] = 'Language';
+$string['gender'] = 'Persona Gender';
+$string['major'] = 'Major of The Lecture';
+$string['lecturevid'] = 'Video';
